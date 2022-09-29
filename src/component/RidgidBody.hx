dcedcd0f449b49138a04b541966a89cf66c0b3eb
Daniel Castro <9056589+ZeroDoctor@users.noreply.github.com>
2022-09-29 19:39:43 +0000

    add optional fields to ridgid body

--- src/component/RidgidBody.hx
+++ src/component/RidgidBody.hx
@@ -3,20 +3,17 @@ package component;
 typedef RidgidBodyDef = {
 	@:optional var mass:Float;
 	@:optional var friction:Float;
-	@:optional var velocity:Float;
 	@:optional var acceleration:Float;
 };
 
 class RidgidBody implements IComponent {
 	public var mass:Float = 1.0;
 	public var friction:Float = 0.25;
-	public var velocity:Float = 0.75;
-	public var acceleration:Float = 0.05;
+	public var acceleration:Float = 0.0;
 
 	public function new(def:RidgidBodyDef):Void {
 		this.mass = def.mass != null ? def.mass : this.mass;
 		this.friction = def.friction != null ? def.friction : this.friction;
-		this.velocity = def.velocity != null ? def.velocity : this.velocity;
 		this.acceleration = def.acceleration != null ? def.acceleration : this.acceleration;
 	}
 
